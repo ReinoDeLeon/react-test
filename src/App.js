f6e7aa098dca311cd8fbf4861abac2b9c2e4ce7c
Ivan Anton Sagastizabal <ianton@d4dom.net>
2023-02-01 13:00:03 +0100

    Added author info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef} from 'react'
 
 function App() {
+  const mainRef = useRef(null);
+      
+
+  const autor = "Iván Antón";
+  
+  useEffect(() => {
+    mainRef.current.innerHTML = "Página desarrollada por " + autor;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2 ref={mainRef}>No js enabled</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
